18ac92ee697772b4f9bcd45ca7e9cf6ce67da404
johnhiu399-code <johnhiu399@gmail.com>
2026-04-29 23:51:50 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -26,7 +26,7 @@ scope = [
     "https://spreadsheets.google.com/feeds",
     "https://www.googleapis.com/auth/drive"
 ]
-creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
+creds = ServiceAccountCredentials.from_json_keyfile_name('/etc/secrets/credentials.json', scope)
 client = gspread.authorize(creds)
 sheet = client.open(SHEET_NAME).sheet1
 
